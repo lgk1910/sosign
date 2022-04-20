ce682280c8d0a1efa44a619beb564f0956ffd305
NGUYEN LUAT GIA KHOI <57819211+lgk1910@users.noreply.github.com>
2022-04-20 10:17:41 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,18 +13,20 @@ let butt = 0
 led.enable(false)
 basic.forever(function () {
     if (NPNBitKit.Button(DigitalPin.P0)) {
+        serial.writeString("!" + "1" + ":" + "LIGHT" + ":" + NPNBitKit.AnalogLight(AnalogPin.P3) + "#")
         if (butt == 0) {
-            butt = 1
-            NPNBitKit.Led2ColorAnalog(AnalogPin.P2, 0, AnalogPin.P1, 100)
+            if (NPNBitKit.AnalogLight(AnalogPin.P3) <= 150) {
+                NPNBitKit.Relay(DigitalPin.P4, true, 100)
+            } else {
+                butt = 1
+                NPNBitKit.Led2ColorAnalog(AnalogPin.P2, 0, AnalogPin.P1, 100)
+                serial.writeString("!" + "1" + ":" + "BUTT" + ":" + butt + "#")
+            }
         } else {
             butt = 0
             NPNBitKit.Led2ColorAnalog(AnalogPin.P2, 100, AnalogPin.P1, 0)
+            serial.writeString("!" + "1" + ":" + "BUTT" + ":" + butt + "#")
         }
-        serial.writeString("!" + "1" + ":" + "BUTT" + ":" + butt + "#")
-    }
-    serial.writeString("!" + "1" + ":" + "LIGHT" + ":" + NPNBitKit.AnalogLight(AnalogPin.P3) + "#")
-    if (NPNBitKit.AnalogLight(AnalogPin.P3) <= 150) {
-        NPNBitKit.Relay(DigitalPin.P4, true, 100)
     }
     basic.pause(1000)
 })
